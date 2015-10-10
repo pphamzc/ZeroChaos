3231344de23872edc1cabc0f83c164e797c0fc7b
ppham@zerochaos.com <github@1970>
2015-10-09 20:12:52 -0700

    Updated Entities and filled out empty entities.

---
+++ Entities/Resource.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeroChaos.Business.Entities
+{
+    public class Resource
+    {
+        public string CareerGoal { get; set; }
+        public string AspiringQuote { get; set; }
+        public string website { get; set; }
+        public string MonthAndDayOfBirth { get; set; }
+        public int UniqueResourceID { get; set; }
+        public string NationalID { get; set; }
+        public string CitizenCountryCode { get; set; }
+        public bool AllowProfileSearch { get; set; }
+        public bool WillingToRelocate { get; set; }
+
+    }
+}
